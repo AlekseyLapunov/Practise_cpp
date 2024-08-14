ee55ce8e0c316db2fb0f96f979e2b69825572b5f
Aleksei Lapunov <lapunoff.alex@yandex.ru>
2024-08-14 20:07:06 +0700

    Update comments

--- Multithreading/14_Std_Future_and_Promise/14_Std_Future_and_Promise/main.cpp
+++ Multithreading/14_Std_Future_and_Promise/14_Std_Future_and_Promise/main.cpp
@@ -13,7 +13,7 @@ void countOddSum(std::promise<uint64_t>&& oddSumPromise, uint64_t start, uint64_
     for (uint64_t i = start; i <= end; i++)
         oddSum += i % 2 ? i : 0;
 
-    oddSumPromise.set_value(oddSum);
+    oddSumPromise.set_value(oddSum); // setting the value
 
     std::cout << "| Thread: finished counting\n";
 }
@@ -23,15 +23,15 @@ int main()
     const uint64_t start = 0;
     const uint64_t end = 1900000000;
 
-    std::promise<uint64_t> oddSum;
-    std::future<uint64_t> oddSumFuture = oddSum.get_future();
+    std::promise<uint64_t> oddSum; // promise is like a setter...
+    std::future<uint64_t> oddSumFuture = oddSum.get_future(); // ...and future is like a getter
 
     std::cout << "Creating thread\n";
     std::thread t(countOddSum, std::move(oddSum), start, end);
 
     std::cout << "Waiting for result\n";
     // Main execution thread will wait for result until it is ready to be claimed
-    std::cout << "oddSum = " << oddSumFuture.get() << "\n";
+    std::cout << "oddSum = " << oddSumFuture.get() << "\n"; // getting the value
 
     std::cout << "Joining thread\n";
     if (t.joinable())
